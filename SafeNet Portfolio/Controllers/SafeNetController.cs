6c476341955e561fff1bddd84b6fbd3f3b848bce
SamzFerg <SamzFerg@gmail.com>
2018-06-26 09:33:20 -0500

    Added SafeNet Controller
    Added Controller for all of the SafeNet Pages

---
+++ SafeNet Portfolio/Controllers/SafeNetController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SafeNet_Portfolio.Controllers
+{
+    public class SafeNetController : Controller
+    {
+        // GET: SafeNet
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
